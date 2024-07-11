ff3c8c30a947fca1c880e5fe083ae86f573d9e55
Tausif-09 <tausifmujawar78@gmail.com>
2024-07-11 22:29:09 +0530

    Next Level

--- src/ex_11072024/LabCode005.java
+++ src/ex_11072024/LabCode005.java
@@ -1,2 +1,11 @@
-package ex_11072024;public class LabCode005 {
+package ex_11072024;
+
+public class LabCode005 {
+
+    public static void main(String[] args) {
+        System.out.println("Ya Ali Madat");
+        System.out.println("Hello Word");
+    }
+
+
 }
