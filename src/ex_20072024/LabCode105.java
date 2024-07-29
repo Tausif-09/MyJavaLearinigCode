98fde3a1ad621679fa35c29940b8a4c36cf20cd0
Tausif-09 <tausifmujawar78@gmail.com>
2024-07-29 17:03:15 +0530

    Loop, condition and Function Home work

--- src/ex_20072024/LabCode105.java
+++ src/ex_20072024/LabCode105.java
@@ -4,6 +4,7 @@ public class LabCode105 {
     public static void main(String[] args) {
 
         char ch = 'a'; //
+        double d = 50;
         if ( ch == 'a' || ch == 'e' || ch =='o' || ch == 'i' || ch == 'u')
         {
             System.out.println("Vowel");
